39f2b8eb2e1a62d3b075a70ac6a215d3a75a043c
Palak Mathur <palakmathur@gmail.com>
2020-10-17 20:56:40 -0500

    Add mdn docs as the search option

--- assets/js/webcmd.js
+++ assets/js/webcmd.js
@@ -2,38 +2,40 @@
 
 // Simple shortcuts: name: url.
 var navigation = {
-     "p":   "http://systemhalted.in/webcmd",
+    "p":   "http://systemhalted.in/webcmd",
     "pi":   "http://systemhalted.in/about",
-   // "pa":   "http://systemhalted.in/articles/",
-   // "pc":   "http://systemhalted.in/contact",
+    // "pa":   "http://systemhalted.in/articles/",
+    // "pc":   "http://systemhalted.in/contact",
     "pr":   "http://systemhalted.in/feed.xml",
     "ph":   "http://systemhalted.in/",
-   "pgh":   "https://github.com/systemhalted"
+    "pgh":   "https://github.com/systemhalted"
 }
 
 var shortcuts = {
-	
+    
     "m":   "https://mail.google.com/",
     "c":   "https://www.google.com/calendar",
     "r":   "http://reddit.com/",
-   "bb":   "http://boingboing.net/",
-   "fb":   "http://facebook.com",
-   "gp":   "http://getpocket.com/",
-  "git":   "http://gitref.org/",
-  "omscs": "http://omscs.gatech.edu/current-courses",
+    "bb":   "http://boingboing.net/",
+    "fb":   "http://facebook.com",
+    "gp":   "http://getpocket.com/",
+    "git":   "http://gitref.org/",
+    "omscs": "http://omscs.gatech.edu/current-courses",
     "n":   "http://netflix.com",
     "q":   "http://www.quora.com"
 }
 
 // Search shortcuts: name: [url, cgiparam, {extra cgi}]
 var searches = {
-	"a":	["http://www.amazon.com/s", "field-keywords",
-			{"url": "search-alias=aps" }],
-	"g":	["http://www.google.com/search", "q"],
-       "gi":	["http://www.google.com/images", "q"],
-	"w":	["http://en.wikipedia.org/wiki/Special:Search", "search"],
-        "f":    ["http://www.flipkart.com/search/a/all", "query",
-            {"vertical": "All+Categories", "affid":"palakmathur"}]
+    "a":	["http://www.amazon.com/s", "field-keywords",
+		 {"url": "search-alias=aps" }],
+    "g":	["http://www.google.com/search", "q"],
+    "gi":	["http://www.google.com/images", "q"],
+    "w":	["http://en.wikipedia.org/wiki/Special:Search", "search"],
+    "f":    ["http://www.flipkart.com/search/a/all", "query",
+	     {"vertical": "All+Categories", "affid":"palakmathur"}],
+    "mdn": ["https://developer.mozilla.org/en-US/search", "q"]
+
 }
 
 // Help text to be displayed for shortcuts & commands.
@@ -50,6 +52,7 @@ var help = {
     "g":	"google search",
     "gi":	"google image search",
     "cs":	"google code search",
+    "mdn": "mdn web docs search",
     "oeis":	"online encyclopedia of integer sequences",
     "m":	"google mail",
     "c":	"google calendar",
@@ -60,7 +63,7 @@ var help = {
     "f":    "flipkart",
     "cls":  "clear output/errors"
     
-   
+    
 }
 
 // Commands: args are command name, arg text,
@@ -71,7 +74,7 @@ var help = {
 // Evaluate an argument.
 function cmd_e(cmd, arg, args)
 {
-	output(arg + " = " + eval(arg));
+    output(arg + " = " + eval(arg));
 }
 
 function cmd_cls(cmd, arg, arg)
@@ -91,167 +94,168 @@ function helptext()
 {
     //alert("helptext");
     var a;
-	var i = 0;
-	var s = "";
-	
-	s += "<table cellspacing=0 cellpadding=0 border=0>";   
-    //alert("1");
-	
-    a = new Array();
-	i = 0;
-	for(var k in navigation)
-		a[i++] = k;
-	//a.sort();
-	//alert("2");
-	s += "<tr><td colspan=3><b>Site Navigation Commands:</b>";
-	
-	for(i=0; i<a.length; i++){	  
-		var h = help[a[i]];        
-		if(h == undefined)
-			h = navigation[a[i]];
-		s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
-	}
-	s += "<tr height=10>\n";
-	
-	a = new Array();
-	i = 0;
-	for(var k in searches)
-		a[i++] = k;
-	a.sort();
-	s += "<tr><td colspan=3><b>Searches:</b>";
-	for(i=0; i<a.length; i++){	     
-		var h = help[a[i]];
-		if(h == undefined)
-			h = searches[a[i]][0];
-        //alert("a[" + i +"]:" + a[i]);
-		s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
-		
-	}
-		
-	s += "<tr height=10>\n";
-
-	a = new Array();
-	i = 0;
-	for(var k in shortcuts)
-		a[i++] = k;
-	a.sort();
-	s += "<tr><td colspan=3><b>Shortcuts:</b>";
-	for(i=0; i<a.length; i++){
-	    var h = help[a[i]];
-		if(h == undefined)
-			h = shortcuts[a[i]];
-		s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
-	}
-	s += "<tr height=10>\n";
-	
-	a = new Array();
-	i = 0;
-	for(var k in window)
-		if(k.substr(0,4) == "cmd_")
-			a[i++] = k.substr(4);
-	a.sort();
-	s += "<tr><td colspan=3><b>Additional Commands:</b>";
-	for(i=0; i<a.length; i++){
-		var h = help[a[i]];
-		if(h == undefined)
-			h = "???";
-		s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
-	}
-	s += "<tr height=10>\n";
-
-	s += "</table>\n";
+    var i = 0;
+    var s = "";
     
-	return s;
+    s += "<table cellspacing=0 cellpadding=0 border=0>";   
+    //alert("1");
+    
+    a = new Array();
+    i = 0;
+    for(var k in navigation)
+	a[i++] = k;
+    //a.sort();
+    //alert("2");
+    s += "<tr><td colspan=3><b>Site Navigation Commands:</b>";
+    
+    for(i=0; i<a.length; i++){	  
+	var h = help[a[i]];        
+	if(h == undefined)
+	    h = navigation[a[i]];
+	s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
+    }
+    s += "<tr height=10>\n";
+    
+    a = new Array();
+    i = 0;
+    for(var k in searches)
+	a[i++] = k;
+    a.sort();
+    s += "<tr><td colspan=3><b>Searches:</b>";
+    for(i=0; i<a.length; i++){	     
+	var h = help[a[i]];
+	if(h == undefined)
+	    h = searches[a[i]][0];
+        //alert("a[" + i +"]:" + a[i]);
+	s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
+	
+    }
+    
+    s += "<tr height=10>\n";
+
+    a = new Array();
+    i = 0;
+    for(var k in shortcuts)
+	a[i++] = k;
+    a.sort();
+    s += "<tr><td colspan=3><b>Shortcuts:</b>";
+    for(i=0; i<a.length; i++){
+	var h = help[a[i]];
+	if(h == undefined)
+	    h = shortcuts[a[i]];
+	s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
+    }
+    s += "<tr height=10>\n";
+    
+    a = new Array();
+    i = 0;
+    for(var k in window)
+	if(k.substr(0,4) == "cmd_")
+	    a[i++] = k.substr(4);
+    a.sort();
+    s += "<tr><td colspan=3><b>Additional Commands:</b>";
+    for(i=0; i<a.length; i++){
+	var h = help[a[i]];
+	if(h == undefined)
+	    h = "???";
+	s += "<tr><td><b>" + a[i] + "</b><td width=10><td>" + h + "\n";
+    }
+    s += "<tr height=10>\n";
+
+    s += "</table>\n";
+    
+    return s;
 }
 
 // Run command.
 function runcmd(cmd)
 {
     //alert("inside runcmd")
-	// Check for URL.
-	var space = cmd.indexOf(' ');
+    // Check for URL.
+    var space = cmd.indexOf(' ');
     
-	if(space == -1 && (cmd.indexOf('/') != -1 || cmd.indexOf('.') != -1)){
-		// No spaces, has slash or dot: assume URL.
-		if(cmd.indexOf('://') == -1)
-			cmd = "http://" + cmd;
-		// window.location = cmd;
-		window.open(cmd);
-		return false;
-	}
-	if(space == -1){
-		arg = "";
-		args = new Array();
-	}else{
-		arg = cmd.substr(space+1);
-		cmd = cmd.substr(0, space);
-		args = toarray(arg.split(/\s+/));
-	}
+    if(space == -1 && (cmd.indexOf('/') != -1 || cmd.indexOf('.') != -1)){
+	// No spaces, has slash or dot: assume URL.
+	if(cmd.indexOf('://') == -1)
+	    cmd = "http://" + cmd;
+	// window.location = cmd;
+	window.open(cmd);
+	return false;
+    }
+    if(space == -1){
+	arg = "";
+	args = new Array();
+    }else{
+	arg = cmd.substr(space+1);
+	cmd = cmd.substr(0, space);
+	args = toarray(arg.split(/\s+/));
+    }
 
-	var fn;
-	if(searches[cmd] != undefined)
-		fn = search;
-	else if(shortcuts[cmd] != undefined)
-		fn = shortcut;
+    var fn;
+    if(searches[cmd] != undefined)
+	fn = search;
+    else if(shortcuts[cmd] != undefined)
+	fn = shortcut;
     else if(navigation[cmd] != undefined)
         fn = navigations;
-	else{
-		fn = window["cmd_" + cmd];
-		if(fn == undefined){
-			error("no command: " + cmd);
-			return false;
-		}
+    else{
+	fn = window["cmd_" + cmd];
+	if(fn == undefined){
+	    error("no command: " + cmd);
+	    return false;
 	}
-   
-	fn(cmd, arg, args);
-	return false;
+    }
+    
+    fn(cmd, arg, args);
+    return false;
 }
 
 // Print output on page.
 function output(s)
 {
-	document.getElementById("output").innerHTML += s + "<br>";
+    document.getElementById("output").innerHTML += s + "<br>";
 }
 
 // Print error on page.
 function error(s)
 {
-	document.getElementById("error").innerHTML += s + "<br>";
+    document.getElementById("error").innerHTML += s + "<br>";
 }
 
 // Convert whatever split returns into an Array.
 function toarray(args)
 {
     var a = new Array();
-	for(var i = 0; i < args.length; i++)
-		a[i] = args[i];
-	return a;
+    for(var i = 0; i < args.length; i++)
+	a[i] = args[i];
+    return a;
 }
 
 // Return a URL with some CGI args.
 function cgiurl(base, params)
 {
     var url = base + "?";
-	for(var k in params)
-		url += k + "=" + escape(params[k]) + "&";
-	return url;
+    for(var k in params)
+	url += k + "=" + escape(params[k]) + "&";
+    return url;
 }
 
 // Handle search shortcut.
 function search(cmd, arg, args)
 {
-	var a = searches[cmd][2];
-	if(a == undefined)
-		a = {};
-	a[searches[cmd][1]] = arg;
-	window.location = cgiurl(searches[cmd][0], a);
+    var a = searches[cmd][2];
+    if(a == undefined)
+	a = {};
+    a[searches[cmd][1]] = arg;
+    window.open(cgiurl(searches[cmd][0], a), "_blank");
+    //window.location = cgiurl(searches[cmd][0], a);
 }
 
 // Handle simple shortcut.
 function shortcut(cmd, arg, args)
 {
     window.open(shortcuts[cmd], "_blank");
-   // window.location = shortcuts[cmd];
+    // window.location = shortcuts[cmd];
 }
 
 function navigations(cmd, arg, args)
@@ -260,5 +264,5 @@ function navigations(cmd, arg, args)
 }
 
 $('a').click(function() {
-  $(this).attr('target', '_blank');
+    $(this).attr('target', '_blank');
 }); 
